724457abc100e1e8003a16154ba93304b3b1c51f
Simon Naarmann <s.naarmann@gmail.com>
2024-12-18 00:42:17 +0100

    Three small ints

--- src/file/key/key.d
+++ src/file/key/key.d
@@ -12,39 +12,35 @@ module file.key.key;
 
 struct Key {
 private:
-    /*
-     * A = 1, B = 2, ..., this runs through all Allegro-mapped keyboard keys.
-     */
+    // A = 1, B = 2, ..., this runs through all Allegro-mapped keyboard keys.
     short _kb;
 
-    /*
-     * 1 = LMB, 2 = RMB, 3 = some third button, ...
-     * -1 = Wheel up
-     * -2 = Wheel down
-     */
-    short _mb;
+    // 1 = LMB, 2 = RMB, 3 = some third button, ...
+    byte _mb;
+
+    // 1 = Wheel up, 2 = Wheel down, >= 3 is
+    byte _wh;
 
 public:
     static Key byA5KeyId(in int a5KeyId) pure nothrow @safe @nogc
     {
-        return Key(a5KeyId & 0x7FFF, 0);
+        return Key(a5KeyId & 0x7FFF, 0, 0);
     }
 
     static Key byMouseButtonId(in int mButtonId) pure nothrow @safe @nogc
     {
-        return Key(0, mButtonId & 0x7FFF);
+        return Key(0, mButtonId & 0x7F, 0);
     }
 
-    enum Key lmb = Key(0, 1);
-    enum Key rmb = Key(0, 2);
-    enum Key mmb = Key(0, 3);
-    enum Key wheelUp = Key(0, -1);
-    enum Key wheelDown = Key(0, -2);
+    enum Key lmb = Key(0, 1, 0);
+    enum Key rmb = Key(0, 2, 0);
+    enum Key mmb = Key(0, 3, 0);
+    enum Key wheelUp = Key(0, 0, 1);
+    enum Key wheelDown = Key(0, 0, 2);
 
-    bool isValid() const pure nothrow @safe @nogc
-    {
-        return (_kb != 0) ^ (_mb != 0);
-    }
+    enum typeof(_kb) firstInvalidKeyboardKey = 240;
+    enum typeof(_kb) firstInvalidMouseButton = 32;
+    enum typeof(_wh) firstInvalidWheelDirection = 3;
 
     enum Type {
         keyboardKey,
@@ -54,8 +50,24 @@ public:
 
     Type type() const pure nothrow @safe @nogc
     {
-        return _mb > 0 ? Type.mouseButton
-            :  _mb < 0 ? Type.mouseWheelDirection : Type.keyboardKey;
+        return _mb != 0 ? Type.mouseButton
+            :  _wh != 0 ? Type.mouseWheelDirection : Type.keyboardKey;
+    }
+
+    bool isValid() const pure nothrow @safe @nogc
+    {
+        if (1 != (_kb != 0) + (_mb != 0) + (_wh != 0)) {
+            // Valid keys have one of _kb, _mb, _wh != 0 and the two others 0.
+            return false;
+        }
+        final switch (type) {
+        case Type.keyboardKey:
+            return _kb >= 0 && _kb < firstInvalidKeyboardKey;
+        case Type.mouseButton:
+            return _mb >= 0 && _mb < firstInvalidMouseButton;
+        case Type.mouseWheelDirection:
+            return _wh >= 0 && _wh < firstInvalidWheelDirection;
+        }
     }
 
     int opCmp(in typeof(this) rhs) const pure nothrow @safe @nogc
@@ -87,10 +99,11 @@ public:
     do { return _mb; }
 
 private:
-    this(short a, short b) pure nothrow @safe @nogc
+    this(short a, byte b, byte c) pure nothrow @safe @nogc
     {
         _kb = a;
         _mb = b;
+        _wh = c;
     }
 }
 
